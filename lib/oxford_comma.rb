32fabd906ce3220920a78ea8a6857f05834324d7
Cameron J Leverett <cameron.leverett@yahoo.com>
2020-02-16 22:53:27 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,8 +6,16 @@ def oxford_comma(array)
   elsif array.length == 1
   array.join
 elsif array.length > 2
-  array[-2].prepend "and "
-array.join(", ")
+  def oxford_comma(array)
+    if array.length == 2
+      array[-2] << " and "
+      array.join
 
-end
- end
+    elsif array.length == 1
+    array.join
+  elsif array.length > 2
+    array[-2].prepend "and "
+  array.join(", ")
+
+  end
+   end
